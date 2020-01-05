8b4d1572c3f700acfdc8a903b9d73e83b15ce163
Alejandro Palomino García <alejandrodh570@gmail.com>
2020-01-05 22:49:31 +0100

    Commit 019 -> Fix en demo de upoteatro_extension

--- upoteatro_extension/__manifest__.py
+++ upoteatro_extension/__manifest__.py
@@ -2,13 +2,13 @@
 {
     'name': "upoteatro_extension",
     'summary': """Extensión del módulo upoteatro""",
-    'description': """NExtensión que añade los modelos: entradaIndividual, entradaGrupal y Autobús para las entradas grupales.""",
+    'description': """Extensión que añade los modelos: entradaIndividual, entradaGrupal y Autobús para las entradas grupales.""",
     'author': "Alejandro Palomino, Álvaro García, Jesús Sena y Juan Alberto García",
     'category': 'teatro',
     'version': '0.1',
     'depends': ['upoteatro'],
     'data': ['views/entradaindividual_view.xml'],
-    'demo': [''],
+    'demo': [],
     #'demo': ['demo/datosPrueba.xml'],
     'application': True,
 }
