ec67addf03465211726a946eb32f802ddc3b5925
Alejandro Palomino García <alejandrodh570@gmail.com>
2020-01-05 21:43:27 +0100

    Commit 017 -> Fix Carpetas a minúsculas y creación de upoteatro_extension

---
+++ upoteatro_extension/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "upoteatro_extension",
+    'summary': """Extensión del módulo upoteatro""",
+    'description': """NExtensión que añade los modelos: entradaIndividual, entradaGrupal y Autobús para las entradas grupales.""",
+    'author': "Alejandro Palomino, Álvaro García, Jesús Sena y Juan Alberto García",
+    'category': 'teatro',
+    'version': '0.1',
+    'depends': ['upoteatro'],
+    'data': ['views/entradaIndividual_view.xml'],
+    'demo': [''],
+    #'demo': ['demo/datosPrueba.xml'],
+    'application': True,
+}
---
+++ upoteatro_extension/models/entradaindividual.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class entradaindividual(models.Model):
+    _name = 'upoteatro_extension.entradaindividual'
+    _rec_name = 'entrada_id'
+
+    tipoEntrada = fields.Selection([('juvenil','Juvenil'),
+                                     ('jubilado','Jubilado'),
+                                     ('infantil','Infantil'),
+                                     ('estudiante','Estudiante'),
+                                     ('normal','Normal'),],'Tipo de Entrada',required=True)
+    entrada_id = fields.Many2one('upoteatro.entrada',string='Entrada',required=True)
