ce62e263c7c1a1265fedc81da79760000014a137
Alejandro Palomino García <alejandrodh570@gmail.com>
2020-01-07 19:39:47 +0100

    Commit 025 -> Añadido entrada grupal

--- upoteatro_extension/models/entrada.py
+++ upoteatro_extension/models/entrada.py
@@ -6,3 +6,4 @@ class entrada(models.Model):
     _inherit = 'upoteatro.entrada'
 
     entradaIndividual_id = fields.Many2one('upoteatro_extension.entradaindividual',string='Entrada Individual')
+    entradaGrupal_id = fields.Many2one('upoteatro_extension.entradagrupal',string='Entrada Grupal')
---
+++ upoteatro_extension/models/entradagrupal.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class entradaindividual(models.Model):
+    _name = 'upoteatro_extension.entradagrupal'
+    _rec_name = 'numeroPersonas'
+    
+    numeroPersonas = fields.Integer('Número de personas',required=True)
