d666f67ebf4fe81e3c3a11695e64d3d765e852ee
Alejandro Palomino García <alejandrodh570@gmail.com>
2020-01-08 05:30:07 +0100

    Commit 027 -> [Da fallos]. Añadidos datos de prueba de upoteatro_extension.

--- upoteatro_extension/models/entrada.py
+++ upoteatro_extension/models/entrada.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api
 
 class entrada(models.Model):
-    _inherit = 'upoteatro.entrada'
+	_inherit = 'upoteatro.entrada'
 
-    entradaIndividual_id = fields.Many2one('upoteatro_extension.entradaindividual',string='Entrada Individual')
-    entradaGrupal_id = fields.Many2one('upoteatro_extension.entradagrupal',string='Entrada Grupal')
+	entradaIndividual_id = fields.Many2one('upoteatro_extension.entradaindividual',string='Entrada Individual')
+	entradaGrupal_id = fields.Many2one('upoteatro_extension.entradagrupal',string='Entrada Grupal')
