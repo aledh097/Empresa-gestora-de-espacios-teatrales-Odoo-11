faccfafb576a41657afac0e9ce361e25d40c11a7
Alejandro Palomino García <alejandrodh570@gmail.com>
2020-01-09 16:25:35 +0100

    Commit 030 -> Autobús se muestra como nº Personas + Matricula

--- upoteatro_extension/models/entradagrupal.py
+++ upoteatro_extension/models/entradagrupal.py
@@ -4,7 +4,14 @@ from odoo import models, fields, api
 
 class entradaindividual(models.Model):
     _name = 'upoteatro_extension.entradagrupal'
-    _rec_name = 'numeroPersonas'
+    _rec_name = 'autobus'
     
     numeroPersonas = fields.Integer('Número de personas',required=True)
     autobus = fields.Many2one('upoteatro_extension.autobus',string='Autobús')
+
+    @api.multi
+    def name_get(self):
+        res = []
+        for record in self:
+            res.append((record.id, "Nº Personas: %s (Autobús: %s)" % (record.numeroPersonas, record.autobus.matricula)))
+        return res
