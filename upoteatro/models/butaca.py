a0cf5785e5942c8df0f3649876ab1da0f6b38094
Alejandro Palomino García <alejandrodh570@gmail.com>
2020-01-10 23:12:45 +0100

    Commit 036 -> Restricción funcional en modelo butaca
    No puede existir más butacas que aforo.

--- upoteatro/models/butaca.py
+++ upoteatro/models/butaca.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+#import logging
+
+#_logger = logging.getLogger(__name__)
 
 class butaca(models.Model):
     _name = 'upoteatro.butaca'
@@ -12,3 +15,10 @@ class butaca(models.Model):
     asiento = fields.Integer('Asiento',required=True)
     teatro_id = fields.Many2one('upoteatro.teatro',string='Teatro',required=True)
     entradas_ids = fields.Many2many('upoteatro.entrada',string='Entradas')
+
+    @api.one
+    @api.constrains('teatro_id')
+    def _check_num_butacas(self):
+    	#_logger.debug("Numero de butacas: " + str(self.teatro_id.aforo) + " Numero de aforo: "  + str(len(self.teatro_id.butacas_ids)))
+    	if(self.teatro_id.aforo < len(self.teatro_id.butacas_ids)):
+    		raise models.ValidationError('No puede existir más butacas que en el aforo del teatro.')
