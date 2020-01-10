348af275683af5778240397f95ec3796bff0c8c0
Alejandro Palomino García <alejandrodh570@gmail.com>
2020-01-10 21:57:09 +0100

    Commit 035 -> Restricción funcional en el modelo entrada
    No se permite asociar una entrada individual a una entrada que ha comprado varias butacas y no se puede asociar una entrada grupal a una entrada que ha comprado una sola butaca.

--- upoteatro_extension/models/entrada.py
+++ upoteatro_extension/models/entrada.py
@@ -7,3 +7,11 @@ class entrada(models.Model):
 
 	entradaIndividual_id = fields.Many2one('upoteatro_extension.entradaindividual',string='Entrada Individual')
 	entradaGrupal_id = fields.Many2one('upoteatro_extension.entradagrupal',string='Entrada Grupal')
+
+	@api.one
+	@api.constrains('butacas_ids','entradaIndividual_id','entradaGrupal_id')
+	def _check_tipo_Entrada(self):
+		if len(self.butacas_ids) > 1 and len(self.entradaIndividual_id)==1:
+			raise models.ValidationError('No puede asignar una entrada individual a una entrada que ha comprado varias butacas.')
+		elif len(self.butacas_ids) == 1 and len(self.entradaGrupal_id)==1:
+			raise models.ValidationError('No puede asignar una entrada grupal a una entrada que ha comprado una sola butaca.')
